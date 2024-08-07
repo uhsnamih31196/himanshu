91e8d9f77e5c41ab4218b544b7dd2badc3e1d4ab
uhsnamih31196 <132296162+uhsnamih31196@users.noreply.github.com>
2024-08-07 17:11:32 +0530

    Update main.tf

--- app-insights/appservide/main.tf
+++ app-insights/appservide/main.tf
@@ -4,21 +4,19 @@ data "azurerm_resource_group" "rg"{
 
 }
 
-resource "azurerm_service_plan" "asp" {
+data "azurerm_service_plan" "dataServicePlan"{
   for_each = var.appService
-  name                = each.value["Name"]
-  resource_group_name = each.value["ResourceGroup"]
-  location            = each.value["Location"]
-  os_type             = each.value["OsType"]
-  sku_name            = each.value["SKU"]
+  name = each.value["service_plan_name"]
+  resource_group_name = ["service_plan_rg"]
 }
+
 
 resource "azurerm_windows_web_app" "app" {
   for_each = var.appService
-  name                = each.value["Name"]
+  name                = each.value["web_appName"]
   resource_group_name = data.azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.asp.location
-  service_plan_id     = azurerm_service_plan.asp.id
+  location            = data.azurerm_service_plan.dataServicePlan.location
+  service_plan_id     = data.azurerm_service_plan.dataServicePlan.id
 
   app_settings = {
 
@@ -26,12 +24,18 @@ resource "azurerm_windows_web_app" "app" {
   }
 
   dynamic "site_config"{
-    for_each each.value["site_config"] != null ? each.value["site_config"] : {}
+    for_each = each.value["site_config"] != null ? each.value["site_config"] : {}
     content {
       always_on = site_config.value["always_on"]
       ftps_state = site_config.value["ftps_state"]
       http2_enabled = site_config.value["http2_enabled"]
-     
+     dynamic "application_stack"{
+      for_each = site_config.value["application_stack"] != null ? site_config.value["application_stack"] : {}
+      content {
+        current_stack  = application_stack.value["current_stack"]
+        dotnet_version = application_stack.value["dotnet_version"]
+      }
+     }
     }
   }
 }

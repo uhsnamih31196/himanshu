63c22cb5ae5d13cbb4178983a48d471ed42178ad
uhsnamih31196 <132296162+uhsnamih31196@users.noreply.github.com>
2024-08-07 17:10:21 +0530

    Update variables.tf

--- app-insights/appservide/variables.tf
+++ app-insights/appservide/variables.tf
@@ -3,15 +3,19 @@ variable "appService" {
   type = map(
     object(
         {
-            Name          = string
-            ResourceGroup = string 
-            Location      = string
-             OsType       = string
+            rg_name          = string
+            service_plan_name = string 
+            service_plan_rg      = string
+             web_appName       = string
              SKU           = string
          site_config = map(object({
           always_on = optional(bool)
           ftps_state = optional(bool)
           http2_enabled = optional(bool)
+          application_stack = map(object({
+           current_stack = string
+            dotnet_version = string
+          }))
          }))    
         }
     )
